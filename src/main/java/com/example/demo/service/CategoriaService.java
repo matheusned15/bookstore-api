4a21f2232a0e60a25340cc0a640aeb8ab783a3fd
matheusned15 <matheusned15@gmail.com>
2021-02-05 02:00:59 -0300

    FindById para Categoria

---
+++ src/main/java/com/example/demo/service/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Categoria;
+import com.example.demo.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+		return obj.orElse(null);
+	}
+}
