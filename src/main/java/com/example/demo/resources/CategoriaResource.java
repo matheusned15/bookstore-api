46c5ad04b3839f9ac2e50871d017babf578ec49b
matheusned15 <matheusned15@gmail.com>
2021-02-06 03:32:50 -0300

    findAll para categoria usando padrao DTO

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -1,5 +1,8 @@
 package com.example.demo.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Categoria;
+import com.example.demo.dtos.CategoriaDTO;
 import com.example.demo.service.CategoriaService;
 
 @RestController
@@ -22,5 +26,12 @@ public class CategoriaResource {
 		Categoria obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 
+	}
+
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findALL() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
