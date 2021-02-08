04d81558ad54c9292af72020dfbafaf64fcc16ec
matheusned15 <matheusned15@gmail.com>
2021-02-07 23:47:11 -0300

    DELETE para Livro

--- src/main/java/com/example/demo/resources/LivroResource.java
+++ src/main/java/com/example/demo/resources/LivroResource.java
@@ -70,9 +70,9 @@ public class LivroResource {
 	}
 
 
-//	@DeleteMapping(value = "/{id}")
-//	public ResponseEntity<Void> delete(@PathVariable Integer id) {
-//		service.delete(id);
-//		return ResponseEntity.noContent().build();
-//	}
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
